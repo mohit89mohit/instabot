9221185e14cc5b6ba76a2acc40916802d108fef1
mohit89mohit <mohitchambial89mohit@gmail.com>
2017-07-11 11:49:53 -0700

    my 2nd commit oninta bot

---
+++ constants.py
@@ -0,0 +1,6 @@
+# global variables file
+
+# instagram API access token
+APP_ACCESS_TOKEN = '2288274672.6d26f8c.5209938e17cd40619b178ddc37793f48'
+# instagram API base url
+BASE_URL = 'https://api.instagram.com/v1/'
